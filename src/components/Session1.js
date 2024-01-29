ac48bddc86e26981587822ff56ad8c71d6eb1fb4
quangphuoc-dev <quangphuoc.dev@gmail.com>
2024-01-29 16:21:06 +0700

    add login.js

--- src/components/Session1.js
+++ src/components/Session1.js
@@ -1,12 +1,18 @@
-            
 import Photo from '../assets/images/photo1.png'
+import BlogItem from './BlogItem';
+
 
 function Session1(){
+    const listPost = [1, 2, 3, 4];
     return <div className="pb-[105px]">
                 <p className="text-[36px] font-bold mb-[8px]">Popular Destinations</p>
                 <p>Vacations to make your experience enjoyable in Indonesia!</p>
                 <div className="mt-[60px] grid grid-cols-4 gap-[30px]">
-                    <div className="">
+
+                    {listPost.map((item) => (
+                        <BlogItem photo={Photo} />
+                    ))}
+                    {/* <div className="">
                         <img src={Photo} alt="" className="aspect-[284/160] w-full "></img>  
                         <p>Flores Road Trip 3D2N</p>
                     </div>
@@ -21,7 +27,7 @@ function Session1(){
                     <div className="">
                         <img src={Photo} alt="" className="aspect-[284/160] w-full "></img>  
                         <p>Flores Road Trip 3D2N</p>
-                    </div>
+                    </div> */}
                 </div>
             </div>
 }
