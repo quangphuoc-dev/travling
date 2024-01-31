1364d540240581c5a2d4b0358b4ef2a26c4dd35e
quangphuoc-dev <quangphuoc.dev@gmail.com>
2024-01-31 16:05:56 +0700

    update router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,41 +1,30 @@
-import { useState } from 'react';
 import Logo from '../assets/images/logo.png';
-import Menu from '../components/Menu';
+import Menu from './Menu';
+import { Link } from 'react-router-dom';
 
-function Header(props){
-    const {title, bgColor} = props;
-    const [count, setCount] = useState(0);
-    const incrementCount = () => {
-      setCount(count + 1);
-    };
-    return (
-        <header className={"h-[112px] w-[1440px] max-w-full flex flex-row items-center justify-between"}>
-        <a
-          href="/"
-        >
-         <img src={Logo} alt="" className='h-[50px] w-[141px]' />
-        </a>
-        <div className='space-x-[80px]'>
-          <Menu href="/product" label="Product"/>
-          <Menu href="/contact" label="Contact"/>
-          <Menu href="/about-us" label="About Us"/>
-          {/* <a href="/product">
-            Product
-          </a>
-          <a href="/contact">
-            Contact
-          </a>
-          <a href='about-us'>
-            About Us
-          </a> */}
-        </div>
-        <button 
-        onClick={incrementCount}
-        className='bg-[#FA8443] rounded-lg h-[46px] w-[120px] text-white'>
-          Sign Up {count}
-        </button>
-      </header>
-    )
+function Header() {
+	return (
+		<header
+			className={
+				'h-[70px] w-[1440px] max-w-full flex flex-row items-center justify-between'
+			}
+		>
+			<a href="/">
+				<img src={Logo} alt="" className="h-[50px] w-[141px]" />
+			</a>
+			<div className="space-x-[80px]">
+				<Menu href="/product" label="Product" />
+				<Menu href="/contact" label="Contact" />
+				<Menu href="/about-us" label="About Us" />
+			</div>
+			<Link
+				to="/login"
+				className="bg-[#FA8443] rounded-lg h-[46px] w-[120px] text-white flex items-center justify-center	"
+			>
+				<span>Sign In</span>
+			</Link>
+		</header>
+	);
 }
 
-export default Header
+export default Header;
