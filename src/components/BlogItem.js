ee9001fbc6fd54061296e74a0313a8f4210822cc
quangphuoc-dev <quangphuoc.dev@gmail.com>
2024-02-22 15:22:10 +0700

    add server

--- src/components/BlogItem.js
+++ src/components/BlogItem.js
@@ -1,12 +1,15 @@
+import { Link } from 'react-router-dom';
 
-
-function BlogItem(props){
-    const {Photo}= props;
-    return (
-        <div className="">
-                <img src={Photo} alt="" className="aspect-[284/160] w-full bg-green-300" ></img>
-                <p>Flores Road trip 3D2N</p>
-        </div>
-    );
+function BlogItem(props) {
+	const { photo, item } = props;
+	return (
+		<div className="">
+			<img src={photo} alt="" className="aspect-[284/160] w-full "></img>
+			<Link to={'/product/' + item.name}>
+				<p>{item.name}</p>
+			</Link>
+		</div>
+	);
 }
+
 export default BlogItem;
